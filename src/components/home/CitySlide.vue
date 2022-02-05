4498f0a7b637bf74afb30627641f2276047b39e6
Leeky0615 <rbdus7174@naver.com>
2022-02-05 20:23:33 +0900

    init

---
+++ src/components/home/CitySlide.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "CitySlide"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/home/PopularTab.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "PopularTab"
+}
+</script>
+
+<style scoped>
+
+</style>
