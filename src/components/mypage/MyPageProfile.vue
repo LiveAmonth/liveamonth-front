e54c58be395be04c09a07547d7418b604b9ff2e0
Leeky0615 <rbdus7174@naver.com>
2022-03-23 18:43:29 +0900

    liveamonth-front-1 [My Page] 마이페이지 화면 UI 구성
    component 구성

---
+++ src/components/mypage/MyPageProfile.vue
@@ -0,0 +1,78 @@
+<template>
+  <section class="profile-section spad">
+    <!--    <div class="container">-->
+    <!--      <div class="profile-agent-content">-->
+    <!--        <div class="row">-->
+    <!--          <div class="col-lg-2">-->
+    <!--            <div class="ih-item circle effect13 top_to_bottom">-->
+    <!--              <a data-toggle="modal" href="#modifyScheduleContentModal">-->
+    <!--                <div class="img">-->
+    <!--                  <img-->
+    <!--                    v-if="image == null"-->
+    <!--                    :src="defaultImg"-->
+    <!--                    class="img-thumbnail img-circle mx-2"-->
+    <!--                  />-->
+    <!--                  <img-->
+    <!--                    v-else-->
+    <!--                    :src="image"-->
+    <!--                    class="img-thumbnail img-circle mx-2"-->
+    <!--                  />-->
+    <!--                </div>-->
+    <!--                <div class="info">-->
+    <!--                  <div class="info-back">-->
+    <!--                    <h3>[[${loginUser.nickname}]]</h3>-->
+    <!--                    <p>프로필 이미지 변경</p>-->
+    <!--                  </div>-->
+    <!--                </div>-->
+    <!--              </a>-->
+    <!--            </div>-->
+    <!--          </div>-->
+    <!--          <div class="col-lg-3">-->
+    <!--            <div class="profile-agent-widget">-->
+    <!--              <ul>-->
+    <!--                <li>[[#{login.name}]] <span>[[${loginUser.name}]]</span></li>-->
+    <!--                <li>-->
+    <!--                  [[#{login.nickname}]] <span>[[${loginUser.nickname}]]</span>-->
+    <!--                </li>-->
+    <!--              </ul>-->
+    <!--            </div>-->
+    <!--          </div>-->
+    <!--          <div class="col-lg-4">-->
+    <!--            <div class="profile-agent-widget">-->
+    <!--              <ul>-->
+    <!--                <li>-->
+    <!--                  [[#{login.age}]]-->
+    <!--                  <span>[[${loginUser.calcAge()}]][[#{age}]]</span>-->
+    <!--                </li>-->
+    <!--                <li>-->
+    <!--                  [[#{login.genderTypes}]]-->
+    <!--                  <span>[[#{'gender.'+${loginUser.genderTypes}}]]</span>-->
+    <!--                </li>-->
+    <!--                <li>[[#{login.email}]] <span>[[${loginUser.email}]]</span></li>-->
+    <!--              </ul>-->
+    <!--            </div>-->
+    <!--          </div>-->
+    <!--        </div>-->
+    <!--      </div>-->
+    <!--    </div>-->
+  </section>
+</template>
+
+<script>
+export default {
+  name: "",
+  components: {},
+  data() {
+    return {
+      simpleData: "",
+    };
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {},
+};
+</script>
+
+<style></style>
---
+++ src/views/mypage/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <!--  <my-page-profile></my-page-profile>-->
+  <side-menu-bar v-if="!isPending"></side-menu-bar>
+  <div>여기에 메인 컨텐츠가 와야함..</div>
+</template>
+
+<script>
+// import MyPageProfile from "@/components/mypage/MyPageProfile";
+import SideMenuBar from "@/components/slot/SideMenuBar";
+import { useMyPage } from "@/composables/myPage";
+
+export default {
+  name: "my-page",
+  components: {
+    // MyPageProfile,
+    SideMenuBar,
+  },
+  setup() {
+    const { error, isPending, getMyPageMenus } = useMyPage();
+    getMyPageMenus();
+    return {
+      error,
+      isPending,
+    };
+  },
+};
+</script>
+
+<style></style>
