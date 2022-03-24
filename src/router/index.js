a0a31f7f94069a572270048d06964f3172c57b89
Leeky0615 <rbdus7174@naver.com>
2022-03-24 14:37:01 +0900

    liveamonth-front-1 [My Page] 마이페이지 화면 UI 구성
    비로그인 접근 제어

--- src/router/index.js
+++ src/router/index.js
@@ -1,36 +1,52 @@
-import { createRouter, createWebHistory } from "vue-router";
+import {createRouter, createWebHistory} from "vue-router";
 import Home from "../views";
 import Login from "@/views/login";
 import MyPage from "@/views/mypage";
+import store from "@/store";
 
 const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/login",
-    name: "Login",
-    component: Login,
-  },
-  {
-    path: "/city",
-    name: "City",
-    component: () =>
-      import(/* webpackChunkName: "Information" */ "@/views/city"),
-    props: true,
-  },
-  {
-    path: "/my-page",
-    name: "MyPage",
-    component: MyPage,
-  },
+    {
+        path: "/",
+        name: "Home",
+        component: Home,
+    },
+    {
+        path: "/login",
+        name: "Login",
+        component: Login,
+    },
+    {
+        path: "/city",
+        name: "City",
+        component: () =>
+            import(/* webpackChunkName: "Information" */ "@/views/city"),
+        props: true,
+    },
+    {
+        path: "/my-page",
+        name: "MyPage",
+        component: MyPage,
+        meta: {requiresAuth: true}
+    },
 ];
+// otherwise redirect to home
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
 });
-
+router.beforeEach((to, from, next) => {
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        const isLogin = store.getters["auth/isLoggedIn"];
+        if (!isLogin) {
+            next({
+                name: "Login",
+            });
+        } else {
+            next();
+        }
+    } else {
+        next();
+    }
+});
 export default router;
--- src/store/modules/auth/index.js
+++ src/store/modules/auth/index.js
@@ -1,4 +1,5 @@
 import AuthService from "@/services/user.service";
+
 /**
  * 1. Server API 호출
  * 2. Response로 사용자정보(토큰)를 받음
@@ -7,43 +8,48 @@ import AuthService from "@/services/user.service";
  */
 const user = JSON.parse(localStorage.getItem("user"));
 const initialState = user
-  ? { status: { loggedIn: true }, user }
-  : { status: { loggedIn: false }, user: null };
+    ? {status: {loggedIn: true}, user}
+    : {status: {loggedIn: false}, user: null};
 
 export default {
-  namespaced: true,
-  state: initialState,
-  // API 호출 값을 파라미터로 전달받은 후 state에 값을 저장
-  mutations: {
-    LOGIN_SUCCESS(state, payload) {
-      state.status.loggedIn = true;
-      state.user = payload;
-    },
-    LOGIN_FAIL(state) {
-      state.status.loggedIn = false;
-      state.user = null;
-    },
-    LOGOUT(state) {
-      state.status.loggedIn = false;
-      state.user = null;
-    },
-  },
-  // Server API 호출
-  actions: {
-    login({ commit }, { username, password }) {
-      return AuthService.login(username, password).then(
-        (user) => {
-          commit("LOGIN_SUCCESS", user);
-          return Promise.resolve(user);
+    namespaced: true,
+    state: initialState,
+    // Server API 호출
+    actions: {
+        login({commit}, {username, password}) {
+            return AuthService.login(username, password).then(
+                (user) => {
+                    commit("LOGIN_SUCCESS", user);
+                    return Promise.resolve(user);
+                },
+                (error) => {
+                    commit("LOGIN_FAIL");
+                    return Promise.resolve(error);
+                }
+            );
         },
-        (error) => {
-          commit("LOGIN_FAIL");
-          return Promise.resolve(error);
-        }
-      );
+        logout({commit}) {
+            AuthService.logout().then(() => commit("LOGOUT"));
+        },
     },
-    logout({ commit }) {
-      AuthService.logout().then(() => commit("logout"));
+    // API 호출 값을 파라미터로 전달받은 후 state에 값을 저장
+    mutations: {
+        LOGIN_SUCCESS(state, payload) {
+            state.status.loggedIn = true;
+            state.user = payload;
+        },
+        LOGIN_FAIL(state) {
+            state.status.loggedIn = false;
+            state.user = null;
+        },
+        LOGOUT(state) {
+            state.status.loggedIn = false;
+            state.user = null;
+        },
     },
-  }
+    getters: {
+        isLoggedIn(state) {
+            return state.status.loggedIn;
+        },
+    }
 };
