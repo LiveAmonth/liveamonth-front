4e8c2ba1bb35c4a47c5b4f7ad7f9dab93dbd5943
Leeky0615 <rbdus7174@naver.com>
2022-03-26 19:00:21 +0900

    fix: add form field input

---
+++ src/components/slot/ErrorField.vue
@@ -0,0 +1,30 @@
+<template>
+  <div v-if="error!== ''">
+    <p class="field-error msg">{{ error }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: String,
+      required: true
+    },
+  },
+}
+
+</script>
+<style scoped>
+.form-group label {
+  text-transform: uppercase;
+  font-weight: 700;
+  font-size: 12px;
+  letter-spacing: 1px;
+  color: #135762;
+}
+
+.msg {
+  color: red;
+}
+</style>
